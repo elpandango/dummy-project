0f1039eae5930cd59f9a657ff1bd2b4e4abda348
Eugen <me@example.com>
2017-03-28 15:55:32 +0300

    Quick start dummy-project

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,45 @@
 var gulp = require('gulp'),
-mmq = require('gulp-merge-media-queries'),
-prefix = require('gulp-autoprefixer'),
-livereload = require('gulp-livereload'),
-connect = require('gulp-connect'),
-concat = require('gulp-concat'),
-sass = require('gulp-sass');
-
-// var path = require('path');
+    mmq = require('gulp-merge-media-queries'),
+    prefix = require('gulp-autoprefixer'),
+    livereload = require('gulp-livereload'),
+    connect = require('gulp-connect'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass'),
+    path = require('path');
 // var concat = require('gulp-concat');
 
 
 //***********************************************************//
-gulp.task('connect', function() {
+gulp.task('connect', function () {
     connect.server({
         root: './',
         livereload: true
     });
 });
 
-gulp.task('sass', function() { // Создаем таск "sass"
-    return gulp.src(['sass/**/*.sass', 'sass/**/*.scss']) // Берем источник
-        .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('css')) // Выгружаем результата в папку css
+gulp.task('sass', function () {
+    return gulp.src(['src/sass/**/*.sass', 'src/sass/**/*.scss'])
+        .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(connect.reload());
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
     return gulp.src('src/js/*.js')
         .pipe(concat('script.js'))
         .pipe(gulp.dest('dist/js'))
         .pipe(connect.reload());
 });
 
-gulp.task('html', function() {
-    return gulp.src('*.html')
+gulp.task('html', function () {
+    return gulp.src('dist/**/*.html')
         .pipe(connect.reload());
 });
 
-// gulp.task('watch', function() {
-//     gulp.watch('src/css/*.css', ['css']);
-//     gulp.watch('src/js/*.js', ['js']);
-//     gulp.watch('*.html', ['html']);
-//     gulp.watch('dist/pages/*.html', ['html']);
-// });
-
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch(['src/sass/**/*.sass', 'src/sass/**/*.scss'], ['sass']);
     gulp.watch('src/js/*.js', ['js']);
-    gulp.watch('*.html', ['html']);
+    gulp.watch('dist/**/*.html', ['html']);
 });
 
 gulp.task('default', ['connect', 'sass', 'js', 'watch']);
